cb55d5834b8c05183e11efea2e0d2febfb9071ab
Brea Zeller <38115861+bzeller7@users.noreply.github.com>
2020-02-04 10:25:23 -0800

    Adding the views and renamed Raffi into Blimpi

--- SCM/Controllers/HomeController.cs
+++ SCM/Controllers/HomeController.cs
@@ -20,6 +20,11 @@ namespace SCM.Controllers
             return View();
         }
 
+        public IActionResult Blimpi()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
