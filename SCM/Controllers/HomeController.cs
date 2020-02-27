f57306b27ed961e8c91db5a01fa91acd4f292dab
Brea Z <That_Silly_Dreamer@hotmail.com>
2020-02-26 19:46:00 -0800

    Added more contents

--- SCM/Controllers/HomeController.cs
+++ SCM/Controllers/HomeController.cs
@@ -30,6 +30,11 @@ namespace SCM.Controllers
             return View();
         }
 
+        public IActionResult Developer()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
